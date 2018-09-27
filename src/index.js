29370d8dded73af023e234aac37801f567e4bb6f
Alexandrina <lizurchik.a@gmail.com>
2018-09-27 22:30:15 +0300

    Add implementation makeExchange function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+module.exports = function makeExchange(num) {
+   let exchange = {};
+  
+      
+  if (num <= 0) {
+    return exchange;
+  }
+    
+  if (num >= 10000) {
+    exchange.error = "You are rich, my friend! We don't have so much coins for exchange"
+     
+    return exchange;
+  }
+  
+  let rate = {
+    H: 50,
+    Q: 25,
+    D: 10,
+    N: 5,
+    P: 1
+  };
+  
+  for (let key in rate) {
+    exchange[key] = getCountOfCoins(num, rate[key]);
+    num -= exchange[key] * rate[key];
+    
+    if (exchange[key] === 0) {
+      delete exchange[key];
+    }
+  }
+    
+  return exchange;
+}
+
+function getCountOfCoins(coins, rate) {
+  return Math.trunc(coins / rate);
 }
